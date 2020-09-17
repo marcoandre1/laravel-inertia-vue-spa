8ad1171af7afeeea97fa895203d2cde35e64cca0
marcoandre1 <marco-andre.echeverria.1@ulaval.ca>
2020-09-17 08:41:30 -0400

    Add Contact page

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -1,13 +1,30 @@
 <template>
-
+    <div>
+        <h1>Contact</h1>
+        <div>
+            <div>
+                <label class="label" for="name">Name</label>
+                <input class="input" id="name" />
+            </div>
+            <div>
+                <label class="label" for="email">Email</label>
+                <input class="input" id="email" />
+            </div>
+            <div>
+                <label class="label" for="description">Description</label>
+                <textarea class="input" id="description" type="text" />
+            </div>
+            <button>
+                <span>Send</span>
+            </button>
+        </div>
+    </div>
 </template>
 
 <script>
+    import Layout from './Layout'
+
     export default {
-        name: "Contact"
+        layout: Layout,
     }
 </script>
-
-<style scoped>
-
-</style>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\HomeController;
 use \App\Http\Controllers\AboutController;
+use \App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,4 @@ use \App\Http\Controllers\AboutController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
